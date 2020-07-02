0536968252b3d5b87c87841d9c147e04621c6587
BrunoCruz95 <brunocruz012013@gmail.com>
2020-07-02 11:34:50 -0300

    Tentativa de conexão com o mongodb

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://receitas-tudo:<brunobenson>@cluster0.odaru.mongodb.net/<receitas-tudo>?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://receitas-tudo:<brunobenson>@cluster0.odaru.mongodb.net/<receitastudo>?retryWrites=true&w=majority"}
 }else{
 module.exports = {mongoURI: "mongodb://localhost/receita-culinaria"}
 }
